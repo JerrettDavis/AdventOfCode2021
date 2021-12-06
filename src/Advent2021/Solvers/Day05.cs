139306f51a92d967ef0b62c40f182fad0b38e2ce
Jerrett Davis <mxjerrett@gmail.com>
2021-12-05 22:30:22 -0600

    Solved day 5 part b

--- src/Advent2021/Solvers/Day05.cs
+++ src/Advent2021/Solvers/Day05.cs
@@ -12,15 +12,49 @@ public class Day05 : ISolver
         var stopwatch = new Stopwatch();
         stopwatch.Start();
         var rows = input.Split(Environment.NewLine);
-        var lines = rows.ToLines()
-            .Where(r => r.IsStraight);
-        var partA = lines
+        var lines = rows.ToLines().ToArray();
+        var straight = lines
+            .Where(r => r.IsStraight)
+            .ToArray();
+        var diagonal = lines
+            .Where(r => r.IsDiagonal)
+            .ToArray();
+        var pointsOnStraight = straight
             .SelectMany(l => GetAllPointsOnStraightLine(l.Start, l.End))
-            .GroupBy(p => new {p.X, p.Y})
+            .ToArray();
+        var pointsOnDiagonal = diagonal
+            .SelectMany(l => GetAllPointsOnDiagonalLine(l.Start, l.End))
+            .ToArray();
+        var partA = pointsOnStraight
+            .GroupBy(p => p)
+            .Count(g => g.Count() > 1);
+        var partB = pointsOnDiagonal
+            .Concat(pointsOnStraight)
+            .GroupBy(p => p)
             .Count(g => g.Count() > 1);
 
         Console.WriteLine($"Took: {stopwatch.Elapsed.TotalMilliseconds}");
-        return new Solution(partA.ToString(), null!);
+
+        return new Solution(partA.ToString(), partB.ToString());
+    }
+
+    private void Print(Dictionary<Point, int> points)
+    {
+        var maxX = points.Max(p => p.Key.X);
+        var maxY = points.Max(p => p.Key.Y);
+
+        for (var y = 0; y <= maxY; y++)
+        {
+            for (var x = 0; x <= maxX; x++)
+            {
+                var point = new Point {X = x, Y = y};
+                if (points.TryGetValue(point, out var value))
+                    Console.Write(value);
+                else
+                    Console.Write(".");
+            }
+            Console.WriteLine();
+        }
     }
 
     private IEnumerable<Point> GetAllPointsOnStraightLine(Point start, Point end)
@@ -42,6 +76,32 @@ public class Day05 : ISolver
                 .Select(r => new Point {X = start.X, Y = r})
             : Enumerable.Range(min, max - min + 1)
                 .Select(r => new Point {X = r, Y = start.Y});
+    }
+
+    private IEnumerable<Point> GetAllPointsOnDiagonalLine(Point start, Point end)
+    {
+        var minX = Math.Min(start.X, end.X);
+        var maxX = Math.Max(start.X, end.X);
+        var range = maxX - minX + 1;
+        var x = Enumerable.Range(minX, range);
+        if (start.X > end.X)
+            x = x.Reverse();
+        
+        var minY = Math.Min(start.Y, end.Y);
+        var y = Enumerable.Range(minY, range);
+        if (start.Y > end.Y)
+            y = y.Reverse();
+        
+        var xArr = x.ToArray();
+        var yArr = y.ToArray();
+
+        var points = new List<Point>();
+        for (var i = 0; i < range; i++)
+        {
+            points.Add(new Point { X = xArr[i], Y = yArr[i]});
+        }
+
+        return points;
     }
 }
 
@@ -80,6 +140,7 @@ public class Line
     public Point End { get; init; } = null!;
 
     public bool IsStraight => Start.X == End.X || Start.Y == End.Y;
+    public bool IsDiagonal => End.X - Start.X != 0 && Math.Abs((End.Y - Start.Y) / (End.X - Start.X)) == 1;
     
     public override string ToString()
     {
