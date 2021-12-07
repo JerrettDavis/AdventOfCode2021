1a6243ad62e94452a1acfb5484c7e56882b8eae2
Jerrett Davis <mxjerrett@gmail.com>
2021-12-06 23:25:41 -0600

    Cleaned up day 7

--- src/Advent2021/Solvers/Day07.cs
+++ src/Advent2021/Solvers/Day07.cs
@@ -15,33 +15,16 @@ public class Day07 : ISolver
 
     private int PartA(IList<int> crabs)
     {
-        var range = Enumerable.Range(crabs.Min(), crabs.Max());
-        var count = range
-            .Select(n => crabs.Sum(crab => Math.Abs(crab - n)));
-
-        return count.Min();
+        
+        return Enumerable.Range(crabs.Min(), crabs.Max())
+            .Select(n => crabs.Sum(crab => Math.Abs(crab - n)))
+            .Min();
     }
 
     private int PartB(IList<int> crabs) {
-        var range = Enumerable.Range(crabs.Min(), crabs.Max());
-        var count = new List<int>();
-        foreach (var n in range)
-        {
-            var c = 0;
-            foreach (var crab in crabs)
-            {
-                var v = Math.Abs(crab - n);
-                var steps = 0;
-                for (var s = 1; s <= v; s++)
-                {
-                    steps += s;
-                }
-                c += steps;
-            }
-
-            count.Add(c);
-        }
-
-        return count.Min();
+        return Enumerable.Range(crabs.Min(), crabs.Max())
+            .Select(n => crabs.Select(crab => Math.Abs(crab - n))
+                .Select(v => v * (v + 1) / 2).Sum())
+            .Min();
     }
 }
