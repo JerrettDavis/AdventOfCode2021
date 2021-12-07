95a4c79ceecdf826be2ea04225a7fc01c33dccbf
Jerrett Davis <mxjerrett@gmail.com>
2021-12-06 23:35:32 -0600

    Just one more tidy up

--- src/Advent2021/Solvers/Day07.cs
+++ src/Advent2021/Solvers/Day07.cs
@@ -8,22 +8,27 @@ public class Day07 : ISolver
 {
     public ISolution Solve(string input)
     {
-        var crabs = input.Split(',').Select(r => r.ToInt()).ToList();
+        var crabs = input
+            .Split(',')
+            .Select(r => r.ToInt())
+            .ToArray();
+        var partA = PartA(crabs);
+        var partB = PartB(crabs);
         
-        return new Solution(PartA(crabs).ToString(), PartB(crabs).ToString());
+        return new Solution(partA.ToString(), partB.ToString());
     }
 
-    private long PartA(IList<int> crabs)
+    private static int PartA(IList<int> crabs)
     {
         return Enumerable.Range(crabs.Min(), crabs.Max())
             .Select(n => crabs.Sum(crab => Math.Abs(crab - n)))
             .Min();
     }
 
-    private long PartB(IList<int> crabs) {
+    private static int PartB(IList<int> crabs) {
         return Enumerable.Range(crabs.Min(), crabs.Max())
             .Select(position => crabs.Select(crab => Math.Abs(crab - position))
-                .Select(steps => (long)steps * (steps + 1)).Sum() / 2)
+                .Select(steps => steps * (steps + 1) / 2).Sum())
             .Min();
     }
 }
