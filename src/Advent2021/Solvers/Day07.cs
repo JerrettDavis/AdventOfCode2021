73c62f62ebd447efc0cd13a9a566a4c2d4c80f0c
Jerrett Davis <mxjerrett@gmail.com>
2021-12-06 23:22:13 -0600

    Quick and dirty day 7

--- src/Advent2021/Solvers/Day07.cs
+++ src/Advent2021/Solvers/Day07.cs
@@ -1,3 +1,4 @@
+using Advent.Common.Extensions;
 using Advent.Common.Interfaces;
 using Advent.Common.Models;
 
@@ -7,7 +8,40 @@ public class Day07 : ISolver
 {
     public ISolution Solve(string input)
     {
+        var crabs = input.Split(',').Select(r => r.ToInt()).ToList();
+        
+        return new Solution(PartA(crabs).ToString(), PartB(crabs).ToString());
+    }
 
-        return new Solution(null!, null!);
+    private int PartA(IList<int> crabs)
+    {
+        var range = Enumerable.Range(crabs.Min(), crabs.Max());
+        var count = range
+            .Select(n => crabs.Sum(crab => Math.Abs(crab - n)));
+
+        return count.Min();
+    }
+
+    private int PartB(IList<int> crabs) {
+        var range = Enumerable.Range(crabs.Min(), crabs.Max());
+        var count = new List<int>();
+        foreach (var n in range)
+        {
+            var c = 0;
+            foreach (var crab in crabs)
+            {
+                var v = Math.Abs(crab - n);
+                var steps = 0;
+                for (var s = 1; s <= v; s++)
+                {
+                    steps += s;
+                }
+                c += steps;
+            }
+
+            count.Add(c);
+        }
+
+        return count.Min();
     }
 }
