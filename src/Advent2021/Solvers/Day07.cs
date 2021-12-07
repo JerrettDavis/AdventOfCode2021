ef02966db697b08892e1431a9027543f97f741f9
Jerrett Davis <mxjerrett@gmail.com>
2021-12-06 22:49:01 -0600

    Prepping day 07

---
+++ src/Advent2021/Solvers/Day07.cs
@@ -0,0 +1,13 @@
+using Advent.Common.Interfaces;
+using Advent.Common.Models;
+
+namespace Advent2021.Solvers;
+
+public class Day07 : ISolver
+{
+    public ISolution Solve(string input)
+    {
+
+        return new Solution(null!, null!);
+    }
+}
